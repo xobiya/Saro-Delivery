60a4ed421c119fefd9de935ae397892d523cad9d
xobiya <eshetufeleke21@gmail.com>
2026-01-03 23:41:14 +0300

    First commit

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: '/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const userInfo = localStorage.getItem('userInfo');
+        if (userInfo) {
+            const { token } = JSON.parse(userInfo);
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export default api;
